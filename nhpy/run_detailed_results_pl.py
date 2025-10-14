19b5d48bcf0534465b0802213f35ff5a84660a7d
Eirini <49712302+ai-mindset@users.noreply.github.com>
2025-10-14 14:36:56 +0100

    fix: spelling

--- nhpy/run_detailed_results_pl.py
+++ nhpy/run_detailed_results_pl.py
@@ -207,7 +207,7 @@ def _process_inpatient_results(
     reference_df = original_df.drop(["speldur", "classpat"])
 
     # Choose a moderate batch size balancing memory usage and performance
-    batch_size = 50
+    batch_size = 30
 
     # Process all runs
     start = time.perf_counter()
