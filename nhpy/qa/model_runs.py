85baba6fca06738ec7e370f1110fd15395e34a73
YiWen Hon <yiwen.hon1@nhs.net>
2026-01-09 15:48:14 +0000

    add loading params to model_runs pipeline

--- nhpy/qa/model_runs.py
+++ nhpy/qa/model_runs.py
@@ -1,6 +1,12 @@
 import argparse
 
-from nhpy.utils import get_logger
+from nhpy.run_full_results import _extract_scenario_components
+from nhpy.utils import (
+    EnvironmentVariableError,
+    _load_environment_variables,
+    _load_scenario_params,
+    get_logger,
+)
 
 logger = get_logger()
 
@@ -19,10 +25,48 @@ def parse_args():
     return parser.parse_args()
 
 
+def load_env_vars():
+    env_config = _load_environment_variables()
+    account_url = env_config["AZ_STORAGE_EP"]
+    container_name = env_config["AZ_STORAGE_RESULTS"]
+    # Check that all required parameters are now set. If not, raise an error.
+    missing_params = [
+        param_name
+        for param_name, param_value in {
+            "account_url": account_url,
+            "container_name": container_name,
+        }.items()
+        if not param_value
+    ]
+    if missing_params:
+        raise EnvironmentVariableError(
+            missing_vars=missing_params,
+            message=f"Missing required parameters: {', '.join(missing_params)}",
+        )
+    return account_url, container_name
+
+
 def main(scenario_filepaths: list[str]):
-    print(scenario_filepaths)
-    # should allow as many scenarios to be added as we want
-    # get params from azure
+    account_url, container_name = load_env_vars()
+
+    existing_params = []
+    for results_path in scenario_filepaths:
+        # Validate the results path format
+        try:
+            _extract_scenario_components(results_path=results_path)
+        except ValueError as e:
+            logger.error(f"run_scenario_with_full_results():Invalid results path: {e}")
+            raise
+
+        existing_params.append(
+            _load_scenario_params(
+                results_path=results_path,
+                account_url=account_url,
+                container_name=container_name,
+            )
+        )
+    print(existing_params)
+    # check app_version consistent across all params to check
     # update params to run on dev instead, change name etc
     # start new model runs on dev using nhp_aci
     # compare default results
